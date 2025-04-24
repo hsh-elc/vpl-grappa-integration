319077ce7e6b2bb71f2d0e825c6d841065caf1f3
Robert Garmann <robert.garmann@hs-hannover.de>
2025-04-24 10:46:04 +0200

    For testing purposes added code to skip TLS certificate verification when contacting GRappa server in the backend.

--- src/main/java/org/abp/vpl_proforma/utility/communicator/GrappaCommunicator.java
+++ src/main/java/org/abp/vpl_proforma/utility/communicator/GrappaCommunicator.java
@@ -6,7 +6,16 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Base64;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
 
 public class GrappaCommunicator implements CommunicatorInterface {
     private final String serviceURL;
@@ -19,6 +28,40 @@ public class GrappaCommunicator implements CommunicatorInterface {
         this.lmsPassword = lmsPassword;
     }
 
+    /**
+     * For testing purposes
+     * Source: https://stackoverflow.com/a/5297100
+     */
+    private static void trustEveryone() {
+        try {
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new X509TrustManager[] { new X509TrustManager() {
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+            } }, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+        } catch (Exception e) { // should never happen
+            e.printStackTrace();
+        }
+    }
+
+    static {
+        // This should not be used in production mode!
+        trustEveryone();
+    }
+    
     /**
      * Checks if the task file identified by the UUID is cached on the server.
      *
